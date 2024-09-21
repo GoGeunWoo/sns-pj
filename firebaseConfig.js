3f19261da7ff4bf08fdb873811fe771f690e8b02
GoGeunWoo <gkw0314@naver.com>
2024-09-21 17:24:35 +0900

    firebase수정

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,13 +1,10 @@
-// Import the functions you need from the SDKs you need
+// Firebase SDK imports
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider, OAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// Firebase configuration
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
@@ -20,10 +17,14 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+// Firebase services
 const auth = getAuth(app);
 const db = getFirestore(app);
 const storage = getStorage(app);
 const googleProvider = new GoogleAuthProvider();
 const appleProvider = new OAuthProvider("apple.com");
 
+// Export 'app' as default, and other services
+export default app; // 'app'을 default로 내보내기
 export { auth, db, storage, googleProvider, appleProvider };
